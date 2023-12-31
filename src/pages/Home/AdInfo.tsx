1cca790b5b48f43c16a2a83dd5ae148b5623e31e
nhat2012782 <gonbbb1@gmail.com>
2023-12-31 17:14:48 +0700

    fix hover chuột chưa hiển thị thông tin điểm đặt

--- src/pages/Home/AdInfo.tsx
+++ src/pages/Home/AdInfo.tsx
@@ -10,8 +10,11 @@ function AdInfo(props: AdInfoProps) {
 
   return (
     <Stack spacing={1} width={200}>
-      <Typography sx={{ fontWeight: 500, fontSize: 16 }}>{info.name}</Typography>
-      <Typography>{info.description}</Typography>
+      <Typography sx={{ fontWeight: 500, fontSize: 16 }}>{info.format}</Typography>
+      <Typography>{info.locationType}</Typography>
+      <Typography sx={{ fontWeight: 500, fontSize: 16, fontStyle: 'italic' }}>
+        {info.planned ? 'Đã quy hoạch' : 'Chưa quy hoạch'}
+      </Typography>
     </Stack>
   );
 }

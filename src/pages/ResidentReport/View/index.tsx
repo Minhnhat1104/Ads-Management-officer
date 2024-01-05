380c799e23922755b97889ccbb2601f5f1c0ad64
MAC <mac@192.168.0.106>
2024-01-05 19:07:31 +0700

    Fix Back btn

--- src/pages/ResidentReport/View/index.tsx
+++ src/pages/ResidentReport/View/index.tsx
@@ -8,6 +8,7 @@ import { Stack, Typography } from '@mui/material';
 import { contractFields } from './config/contractFields';
 import Write from '../Write';
 import { advertisementFields } from './config/advertisementFields';
+import Toolbar from './Toolbar';
 
 const View = () => {
   const { id } = useParams();
@@ -16,41 +17,40 @@ const View = () => {
   const { data } = useReportByReportId(id);
 
   return (
-    <Stack spacing={1} py={1}>
-      {/* report field */}
-      <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Thông tin người báo cáo</Typography>
-      <ViewFields data={data} fieldConfigs={reportFields} />
-
-      {/* placement field */}
-      {data?.placement !== null ? (
-        <>
-          <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Địa điểm</Typography>
-          <ViewFields data={data?.placement} fieldConfigs={placementFields} />
-        </>
-      ) : null}
-
-      {/* advertisement field */}
-      {data?.advertisement !== null ? (
-        <>
-          <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Bảng quảng cáo</Typography>
-          <ViewFields data={data?.advertisement} fieldConfigs={advertisementFields} />
-        </>
-      ) : (
-        <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Không có bảng quảng cáo</Typography>
-      )}
-
-      {/* contract field */}
-      {data?.contract !== null ? (
-        <>
-          <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Hợp đồng quảng cáo</Typography>
-          <ViewFields data={data?.contract} fieldConfigs={contractFields} />
-        </>
-      ) : (
-        <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Không có hợp đồng quảng cáo</Typography>
-      )}
-
-      <Write id={id || ''} />
-    </Stack>
+    <>
+      <Toolbar />
+      <Stack spacing={1} py={1}>
+        {/* report field */}
+        <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Thông tin người báo cáo</Typography>
+        <ViewFields data={data} fieldConfigs={reportFields} />
+        {/* placement field */}
+        {data?.placement !== null ? (
+          <>
+            <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Địa điểm</Typography>
+            <ViewFields data={data?.placement} fieldConfigs={placementFields} />
+          </>
+        ) : null}
+        {/* advertisement field */}
+        {data?.advertisement !== null ? (
+          <>
+            <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Bảng quảng cáo</Typography>
+            <ViewFields data={data?.advertisement} fieldConfigs={advertisementFields} />
+          </>
+        ) : (
+          <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Không có bảng quảng cáo</Typography>
+        )}
+        {/* contract field */}
+        {data?.contract !== null ? (
+          <>
+            <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Hợp đồng quảng cáo</Typography>
+            <ViewFields data={data?.contract} fieldConfigs={contractFields} />
+          </>
+        ) : (
+          <Typography sx={{ fontSize: 16, fontWeight: 500 }}>Không có hợp đồng quảng cáo</Typography>
+        )}
+        <Write id={id || ''} />
+      </Stack>
+    </>
   );
 };
 
